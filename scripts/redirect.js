4260307a7ee7f214852e110221d25a4932c3ebe4
Xavi002 <diogomartins200214@gmail.com>
2021-01-19 23:09:55 +0000

    Article page, missing commentary

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,10 +1,5 @@
     document.getElementById("article-redirect").onclick = function () {
-        location.href = "/article-no-auth.html";
+        location.href = "/article-auth.html";
     };
     
     document.getElementById('leitor').onclick = changeColor;   
-    
-    function changeColor() {
-        document.body.style.color = "#C62828";
-        return false;
-    }   
