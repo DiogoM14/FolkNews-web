4bf7e5a05ae24c417d82e3b9e35bd1d7f715db31
Xavi002 <diogomartins200214@gmail.com>
2021-01-19 16:20:34 +0000

    user type screen finished

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,3 +1,10 @@
     document.getElementById("article-redirect").onclick = function () {
         location.href = "/article-1.html";
     };
+    
+    document.getElementById('leitor').onclick = changeColor;   
+    
+    function changeColor() {
+        document.body.style.color = "#C62828";
+        return false;
+    }   
