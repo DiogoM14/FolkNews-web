067bc49347307440fd71a962928ad9a4587f1b98
Xavi002 <diogomartins200214@gmail.com>
2021-01-25 23:02:27 +0000

    Updates

--- scripts/redirect_auth.js
+++ scripts/redirect_auth.js
@@ -1,3 +1,7 @@
 document.getElementById("auth").onclick = function () {
     location.href = "/article-auth.html";
 };
+
+document.getElementById("auth-legionela").onclick = function () {
+    location.href = "/article-auth-legionela.html";
+};
